b0ae4a42a2f4edcc6be678294a52c93e9ada28ab
Isaque Carlos da Silva Melo <isaquecarlos2016@gmail.com>
2026-04-14 14:28:29 -0300

    criando os repositorios que falta

---
+++ src/main/java/com/senac/tsi/FichasRPG/repositories/MesasRPGRepository.java
@@ -0,0 +1,7 @@
+package com.senac.tsi.FichasRPG.repositories;
+
+import com.senac.tsi.FichasRPG.domains.campanhas.MesasRPG;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MesasRPGRepository extends JpaRepository<MesasRPG,Long> {
+}
